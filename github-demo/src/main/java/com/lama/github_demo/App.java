e8f11d54851e694457488cd7327ebb4774f7dff9
binayl <45529060+binayl@users.noreply.github.com>
2018-12-02 12:46:24 +0530

    Change's done by Vivek Jha

--- github-demo/src/main/java/com/lama/github_demo/App.java
+++ github-demo/src/main/java/com/lama/github_demo/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!"+" from Lama" );
+        System.out.println("changes done by Vivek Jha");
+        System.out.println( "Hello World!"+" from Binay Lama" );
     }
 }
